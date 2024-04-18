061fd07e1509e649d29ca46b1da6757377db1ff5
ShrewdLuni <ander113q@gmail.com>
2024-04-19 00:31:57 +0300

    refactor: merge ProcessImage functions into one function

--- app.go
+++ app.go
@@ -7,9 +7,7 @@ import (
 	"image/color"
 	"image/jpeg"
 	"image/png"
-	"io"
 	"math"
-	"mime/multipart"
 	"net/http"
 	"os"
 	"strconv"
@@ -59,8 +57,11 @@ func main() {
 			return
 		}
 
-		var data []byte
+		var img image.Image
 		var dataError error
+
+		image.RegisterFormat("jpeg", "\xff\xd8", jpeg.Decode, jpeg.DecodeConfig)
+		image.RegisterFormat("png", "\x89PNG\r\n\x1a\n", png.Decode, png.DecodeConfig)
 
 		if req.FormValue("isFile") == "true" {
 			file, _, err := req.FormFile("image")
@@ -69,7 +70,12 @@ func main() {
 				res.WriteHeader(http.StatusBadRequest)
 				return
 			}
-			data, dataError = json.Marshal(ProcessImageFile(file, resolution, brightness))
+			img, _, dataError = image.Decode(file)
+			if dataError != nil {
+				fmt.Println("Error: Image could not be decoded")
+				res.WriteHeader(http.StatusBadRequest)
+				return
+			}
 		} else {
 			imageResponse, err := http.Get(req.FormValue("link"))
 			if err != nil || imageResponse.StatusCode == 201 {
@@ -78,10 +84,16 @@ func main() {
 				return
 			}
 			defer imageResponse.Body.Close()
-			data, dataError = json.Marshal(ProcessImageLink(imageResponse.Body, resolution, brightness))
+			img, _, dataError = image.Decode(imageResponse.Body)
+			if dataError != nil {
+				fmt.Println("Error: Image could not be decoded")
+				res.WriteHeader(http.StatusBadRequest)
+				return
+			}
 		}
 
-		if dataError != nil {
+		data, err := json.Marshal(ProcessImage(img, resolution, brightness))
+		if err != nil {
 			fmt.Printf("Data error: \"%s\"\n", err)
 			res.WriteHeader(http.StatusBadRequest)
 			return
@@ -94,19 +106,13 @@ func main() {
 		port = "3000"
 	}
 
-	if err := http.ListenAndServe("0.0.0.0:"+port, handler); err != nil {
+	if err := http.ListenAndServe("localhost:3001", handler); err != nil {
 		fmt.Println(err.Error())
 	}
 
 }
 
-func ProcessImageFile(file multipart.File, resolution int, brightness float64) []ColorfulAscii {
-	image.RegisterFormat("jpeg", "\xff\xd8", jpeg.Decode, jpeg.DecodeConfig)
-	image.RegisterFormat("png", "\x89PNG\r\n\x1a\n", png.Decode, png.DecodeConfig)
-	img, _, err := image.Decode(file)
-	if err != nil {
-		fmt.Println("Error: Image could not be decoded")
-	}
+func ProcessImage(img image.Image, resolution int, brightness float64) []ColorfulAscii {
 	resolution = Limit(resolution, 1000, 1)
 	bounds := img.Bounds()
 	width, height := bounds.Max.X, bounds.Max.Y
@@ -123,45 +129,7 @@ func ProcessImageFile(file multipart.File, resolution int, brightness float64) [
 			g := float64(color.G)
 			b := float64(color.B)
 			pixelBrightness := ((r + g + b) / 3)
-			sum := (float64(Limit(int(pixelBrightness*brightness), 255, 1)) / 255) * 65
-
-			item := ColorfulAscii{Ascii: string(ASCIIbyBrightness[int(math.Round(sum))]), Color: PixelColor{R: LimitPixel(r * brightness), G: LimitPixel(g * brightness), B: LimitPixel(b * brightness)}}
-			if item.Ascii == "." {
-				item.Color.R = 255
-				item.Color.G = 255
-				item.Color.B = 255
-			}
-			result = append(result, item)
-		}
-		result = append(result, ColorfulAscii{Ascii: string("enter"), Color: PixelColor{R: int(0), G: int(0), B: int(0)}})
-	}
-	return result
-}
-
-func ProcessImageLink(link io.ReadCloser, resolution int, brightness float64) []ColorfulAscii {
-	image.RegisterFormat("jpeg", "\xff\xd8", jpeg.Decode, jpeg.DecodeConfig)
-	image.RegisterFormat("png", "\x89PNG\r\n\x1a\n", png.Decode, png.DecodeConfig)
-	img, _, err := image.Decode(link)
-	if err != nil {
-		fmt.Println("Error: Image could not be decoded")
-	}
-	resolution = Limit(resolution, 1000, 1)
-	bounds := img.Bounds()
-	width, height := bounds.Max.X, bounds.Max.Y
-	ratio := float64(height) / float64(width)
-	width = resolution
-	height = int(float64(float64(width)/1.5) * float64(ratio))
-	img = resize.Resize(uint(width), uint(height), img, resize.Lanczos3)
-	result := make([]ColorfulAscii, width*height)
-	for y := 0; y < height; y++ {
-		for x := 0; x < width; x++ {
-			pixel := img.At(x, y)
-			color := color.RGBAModel.Convert(pixel).(color.RGBA)
-			r := float64(color.R)
-			g := float64(color.G)
-			b := float64(color.B)
-			pixelBrightness := ((r + g + b) / 3)
-			sum := (float64(Limit(int(pixelBrightness*brightness), 255, 1)) / 255) * 65
+			sum := (float64(Limit(int(pixelBrightness*brightness), 255, 1)) / 255) * float64(len(ASCIIbyBrightness)-1)
 
 			item := ColorfulAscii{Ascii: string(ASCIIbyBrightness[int(math.Round(sum))]), Color: PixelColor{R: LimitPixel(r * brightness), G: LimitPixel(g * brightness), B: LimitPixel(b * brightness)}}
 			if item.Ascii == "." {
